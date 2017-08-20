d198deafc6a71d0d570a92bb3d182489f11cb550
Diego Javier Mena Amado <glud@localhost.localdomain>
2017-08-20 11:06:57 -0500

    se altera el codigo para escribir en formato de archivo.csv con los datos separados por comas

--- src/microSD.cpp
+++ src/microSD.cpp
@@ -17,14 +17,27 @@ microcontrolador    microSD
  /***** microSD ******/
 File myFile;
 
-void configMicroSD(){
+void configSerial(){
   Serial.begin(9600);
-  Serial.print("Iniciando SD ...");
-  if (!SD.begin(4)) {
+  Serial.println("Iniciando comunicacion serial ...");
+}
+
+void configMicroSD(){
+  if (!SD.begin(4)){ //La declaracion del pin CS puede ser cualquier pinDigital
     Serial.println("No se pudo inicializar");
     return;
   }
-  Serial.println("inicializacion exitosa");
+  Serial.println("Inicializacion exitosa de la microSD");
+
+  if(!SD.exists("dataset.csv")){
+      myFile = SD.open("dataset.csv", FILE_WRITE);
+      if (myFile) {
+        myFile.println("Tiempo(ms),Sensor1,Sensor2,Sensor3"); // Escribe la primer fila del archivo
+        myFile.close();
+      } else {
+        Serial.println("Error creando el archivo dataset.csv");
+      }
+  }
 }
  /****** FIN FUNCIONES ADICIONALES AL SETUP******/
 
@@ -32,6 +45,7 @@ void configMicroSD(){
   *              SETUP CONFIGURACIÓN INICIAL
   ***********************************************************************/
   void setup() {
+    configSerial();
     configMicroSD();
 }
 
@@ -41,33 +55,29 @@ void configMicroSD(){
  *              LOOP BUCLE PRINCIPAL
  ***********************************************************************/
 void loop() {
-  myFile = SD.open("tomalo.txt", FILE_WRITE);//abrimos  el archivo
+  myFile = SD.open("dataset.csv", FILE_WRITE);//abrimos  el archivo
+
   if (myFile) {
         Serial.print("Escribiendo SD: ");
         int sensor1 = analogRead(0);
         int sensor2 = analogRead(1);
         int sensor3 = analogRead(2);
-        myFile.print("Tiempo(ms)=");
+
         myFile.print(millis());
-        myFile.print(", sensor1=");
+        myFile.print(",");
         myFile.print(sensor1);
-        myFile.print(", sensor2=");
+        myFile.print(",");
         myFile.print(sensor2);
-        myFile.print(", sensor3=");
+        myFile.print(",");
         myFile.println(sensor3);
 
         myFile.close(); //cerramos el archivo
 
-        Serial.print("Tiempo(ms)=");
-        Serial.print(millis());
-        Serial.print(", sensor1=");
-        Serial.print(sensor1);
-        Serial.print(", sensor2=");
-        Serial.print(sensor2);
-        Serial.print(", sensor3=");
-        Serial.println(sensor3);
+        Serial.println("Tiempo(ms)=" + String(millis()) + ", sensor1=" + String(sensor1)
+                        + ", sensor2=" + String(sensor2) + ", sensor3=" + String(sensor3));
+
     } else {
     Serial.println("Error al abrir el archivo");
     }
-  delay(100);
+  delay(1000); //Escribe en la tabla cada 1s
 }
